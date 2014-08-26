06d9787e3852c68b0db68d2e85c5affb596dd200
Gre3nLioN <gre3nlion@gmail.com>
2014-08-26 20:46:34 -0300

    add microfono

---
+++ Arduino/Ejemplos/Microfono/Microfono.ino
@@ -0,0 +1,13 @@
+int soundSensorPin=A0; //pin al que conectamos el Microfono
+int soundReading=0;
+
+void setup(){
+ Serial.begin(9600);
+}
+
+void loop(){
+ soundReading=analogRead(soundSensorPin);
+ //if(soundReading > 34){
+   Serial.println(soundReading);
+ //}
+}
